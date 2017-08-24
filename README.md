f7afa46fe14f78dddb58327954c86eea4695d9b6
iRayan7 <Rayan@Rayans-MacBook-Pro.local>
2017-08-25 02:46:46 +0300

    v0.1
    first alpha.

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-frontend-nanodegree-arcade-game
-===============================
+# Arcade Game
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+A game made for Udacity FE Nanodegree.
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+## How To Run
+just open `index.html` and have fun!
+
+## how to play
+you move the hero to the river on the other side, and beware of the monsters on your way.
+use the arrows on your keyboard to move the hero.
--- js/app.js
+++ js/app.js
@@ -1,40 +1,74 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function (y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = -200;
+    this.y = y;
+    this.speed = speed;
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+Enemy.prototype.update = function (dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if ((this.x > player1.x - 85 && this.x < player1.x + 85) && this.y > player1.y - 50 && this.y < player1.y + 50) {
+        player1.x = 202;
+        player1.y = 404;
+    }
+
+    if (this.x > 700)
+        this.x = -200;
+    this.x = this.x + (this.speed * dt);
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function () {
 
+    this.sprite = "images/char-boy.png";
+    this.x = 101;
+    this.y = 404;
+};
 
+Player.prototype.update = function (dt) {
+
+    Player.x = 50;
+    Player.y = 50;
+
+};
+
+Player.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 // Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+var player1 = new Player();
+var enemy1 = new Enemy(60, 200);
+var enemy2 = new Enemy(145, 100);
+var enemy3 = new Enemy(230, 300);
 
+
+// Place all enemy objects in an array called allEnemies
+var allEnemies = [enemy1, enemy2, enemy3];
+
+// Place the player object in a variable called player
+var player = player1;
 
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
@@ -44,3 +78,23 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+Player.prototype.handleInput = function (keyCode) {
+    if (keyCode === 'left') {
+        if (this.x - 101 > -100)
+            this.x -= 101;
+    } else if (keyCode === 'right') {
+        if (this.x + 101 < 504)
+            this.x += 101;
+    } else if (keyCode === 'up') {
+        if (this.y - 85 > 0)
+            this.y -= 85;
+        else {
+            this.y = 404;
+            this.x = 202;
+        }
+    } else if (keyCode === 'down') {
+        if (this.y + 85 < 406)
+            this.y += 85;
+    }
+};
